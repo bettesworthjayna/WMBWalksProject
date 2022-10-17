1016a0dda0833635b44b7c7128116d4f6eb1c5f3
Jayna Bettesworth <JBettesworth@wmbeck.com>
2022-10-17 11:38:19 -0700

    Connected minting tokens to strava run info

--- FrontEnd/src/pages/YourDistance.js
+++ FrontEnd/src/pages/YourDistance.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { handleLogin } from "../utils/handleLogin";
+import { mineTokens } from "../utils/mineTokens";
 
-const YourDistance = (props) => {
+const YourDistance =  (props) => {
+    // const runWalkDistance = Math.floor(props.user.data.all_run_totals.distance / 10)/100;
+     const [returnMessage, setReturnMessage] = useState('');
+     const [runWalkDistance, setRunWalkDistance] = useState(0);
+        useEffect( () => {
+            async function fetchData (){
+                try{
+                    setRunWalkDistance ( Math.floor(props.user.data.all_run_totals.distance / 10)/100);
+                    console.log(runWalkDistance );
+                    setReturnMessage (await mineTokens(Math.floor(props.user.data.all_run_totals.distance / 10)));
+                }catch(err){
+                    console.log(err);
+                    console.log("!!!!!!!");
+                }
+            }
+            
+            fetchData();
+            console.log(runWalkDistance + "hi there")
+        }, []);
+    console.log(runWalkDistance + '   !!')
     try{
+
+        // const runWalkDistance = Math.floor(props.user.data.all_run_totals.distance / 10)/100;
+        // useEffect(async () => {
+        //     returnMessage = await mineTokens(runWalkDistance);
+        // }, []);
+        
+        
+
     return (
         
         <div>
             <h1>Hi, {props.returnTokens.athlete.firstname}!</h1>
-            <h2>Run / Walk total distance: {props.user.data.all_run_totals.distance}</h2>
+            <h2>Run / Walk total distance: {runWalkDistance}</h2>
             <h2>Bike ride total distance: {props.user.data.all_ride_totals.distance}</h2>
             <h2>Swimming total distance: {props.user.data.all_swim_totals.distance}</h2>
+            <h3>{returnMessage}</h3>
         </div>
        
     );
---
+++ FrontEnd/src/utils/mineTokens.js
@@ -0,0 +1,30 @@
+import token from '../EtherConnect/token';
+import web3 from '../EtherConnect/web3';
+
+
+export const mineTokens = async (amount) => {
+    const accounts = await web3.eth.getAccounts();
+    const OGBalance = await token.methods.balanceOf(accounts[0]).call();
+    const mineAmount = amount - OGBalance;
+    if (mineAmount > 0){
+        await token.methods.mint(amount).send({
+            from: accounts[0],
+        });
+    }else{
+        return "No new km recorded since last logged in. "
+    }
+    const AfterBalance = await token.methods.balanceOf(accounts[0]).call();
+    if(AfterBalance < 10000){
+        return `Congrats, you have ran ${mineAmount} km since you've last checked in`
+    }
+    if(AfterBalance > 10000 && OGBalance < 10000){
+        return "Congrats! You have walked more than 100 total! You now have a free waterbottle NFT and can cash it in at any time"
+    }
+    if (AfterBalance > 50000 && OGBalance <50000){
+        return "Congrats! You have walked more than 500 total! You now have a free comfy socks NFT and can cash it in at any time!"
+    }
+    if (AfterBalance > 100000 && OGBalance < 100000){
+        return "Congrats! You have walked more than 1000 total! You now have a free lunch NFT and can cash it in at any time! "
+    }
+    return `Congrats, you have walked ${mineAmount} since you last logged in`
+}
